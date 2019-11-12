d23cb35a9d93a653703d13c0fc3d54acbc7a3022
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-11-12 02:12:41 +0100

    Init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Character Generator - Raspberry Pi
---
+++ assets/js/firebase.js
@@ -0,0 +1,11 @@
+const initFirebase = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyA3XHcX91KtZ6iEvPNq8fbIhstFZ2pdjng",
+        authDomain: "pi-charactergenerator.firebaseapp.com",
+        databaseURL: "https://pi-charactergenerator.firebaseio.com",
+        projectId: "pi-charactergenerator",
+        storageBucket: "pi-charactergenerator.appspot.com",
+        messagingSenderId: "984679984646",
+        appId: "1:984679984646:web:5c6c3c57e9128eded61d87"
+    };
+}
