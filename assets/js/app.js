d2b9531d218eabb50d9be37488ea34c15525b340
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-11-12 18:05:56 +0100

    Added random arcade character

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,8 +14,23 @@ const generateGrid = (count) => {
     }
 }
 
+const generateRandomCharacter = () => {
+    const rows = document.querySelectorAll('.grid .row');
+
+    rows.forEach(row => {
+        const cells = row.children;
+        for(let i = 0; i < (cells.length / 2); i++) {
+            if(Math.random() > 0.5) {
+                cells[i].classList.add('active');
+                cells[cells.length - 1 - i].classList.add('active');
+            }
+        }
+    })
+}
+
 const init = () => {
     generateGrid(8);
+    generateRandomCharacter();
 }
 
 init();
