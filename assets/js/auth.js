1517db1ba9e71975580c08914e8862d2a1961fda
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-11-13 23:26:01 +0100

    Enabled auth

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,51 +1,51 @@
-// const onLogin = async () => { 
-//     const email = document.querySelector('#email');
-//     const password = document.querySelector('#password');
+const onLogin = async () => { 
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
 
-//     await firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-//         .then(res => {
-//             // Set Unique User ID in LocalStorage
-//             setLocalStorageItem('uuid', res.user.uid);
-//             // Redirect to Home after succesful login
-//             redirectToRoute('/');
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         });
-// }
+    await firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+        .then(res => {
+            // Set Unique User ID in LocalStorage
+            setLocalStorageItem('uuid', res.user.uid);
+            // Redirect to Home after succesful login
+            redirectToRoute('/');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
-// const redirectToRoute = (route) => {
-//     window.location.pathname = route;
-//     console.log(window.location.pathname);
-// }
+const redirectToRoute = (route) => {
+    window.location.pathname = route;
+}
 
-// const setLocalStorageItem = (key, data) => {
-//     window.localStorage.setItem(key, data);
-// } 
+const setLocalStorageItem = (key, data) => {
+    window.localStorage.setItem(key, data);
+} 
 
-// const getLocalStorageItem = (key) => {
-//     return window.localStorage.getItem(key);
-// }
+const getLocalStorageItem = (key) => {
+    return window.localStorage.getItem(key);
+}
 
-// const removeLocalStorageItem = (key) => {
-//     window.localStorage.removeItem(key);
-// }
+const removeLocalStorageItem = (key) => {
+    window.localStorage.removeItem(key);
+}
 
-// firebase.auth().onAuthStateChanged(user => {
-//     if(user) {
-//         const token = getLocalStorageItem('uuid');
-//         if(window.location.pathname == '/') {
-//             if(!token) {
-//                 redirectToRoute('/login.html')
-//             }
-//         }
-//     } else {
-//         redirectToRoute('/login.html')
-//     }
-// })
+firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+        const token = getLocalStorageItem('uuid');
+        console.log(window.location.pathname);
+        // if(window.location.pathname == '/') {
+        //     if(!token) {
+        //         redirectToRoute('/login.html')
+        //     }
+        // }
+    } else {
+        // redirectToRoute('/login.html')
+    }
+})
 
-// const loginBtn = document.querySelector('#loginBtn');
+const loginBtn = document.querySelector('#loginBtn');
 
-// if(loginBtn) {
-//     loginBtn.addEventListener('click', onLogin);
-// }
+if(loginBtn) {
+    loginBtn.addEventListener('click', onLogin);
+}
